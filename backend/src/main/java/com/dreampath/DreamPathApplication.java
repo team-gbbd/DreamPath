a238771bfaa307d04d0da87f130eca2a94eb586f
foeru <yerilotti@gmail.com>
2025-11-14 12:15:01 +0900

    프로젝트 기본 설정

---
+++ backend/src/main/java/com/dreampath/DreamPathApplication.java
@@ -0,0 +1,13 @@
+package com.dreampath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamPathApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dreampath/DreamPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreampath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
