cc5c3f07e5ce8c27d7d3d1e907889b8b03881f0f
foeru <yerilotti@gmail.com>
2025-11-25 22:19:23 +0900

    SQL 파일 정리

--- backend/fix-remaining-sessions.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE users
-ADD COLUMN IF NOT EXISTS remaining_sessions INTEGER DEFAULT 0 NOT NULL;
-
--- 기존 NULL 값들을 0으로 업데이트
-UPDATE users
-SET remaining_sessions = 0
-WHERE remaining_sessions IS NULL;
